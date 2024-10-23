60ce352b2ce281463118babdaf83026cc1dd2dab
harperanthony <harperanthony9@outlook.com>
2024-10-23 18:46:01 +0000

    update

--- setup.sh
+++ setup.sh
@@ -1,27 +1,11 @@
-#!/bin/bash
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
 
-# Update package list
-sudo apt update
+# Verify installation
+java -version # this should output "1.8.X_XXX"
+# If you are still seeing a wrong Java version, you may use
+# the following line to update it
+# sudo update-alternatives --config java
 
-# Install Java
-sudo apt install -y openjdk-11-jdk
-
-# Install Python and pip
-sudo apt install -y python3 python3-pip
-
-# Install system dependencies for MineRL
-sudo apt install -y xvfb ffmpeg
-
-# Upgrade pip
-pip3 install --upgrade pip
-
-# Install Python dependencies
-pip3 install -r requirements.txt
-
-# Install MineRL
-pip3 install git+https://github.com/minerllabs/minerl
-
-# Set up virtual display for headless mode
-echo "export DISPLAY=:0" >> ~/.bashrc
-source ~/.bashrc
-
+pip install git+https://github.com/minerllabs/minerl
